6aec065cc0aef4f459d4d502c63f4640c2f794b9
manalsolanki <97manal@gmail.com>
2021-08-23 17:34:57 -0400

    Added the CSS

--- test.js
+++ test.js
@@ -1,14 +1,21 @@
+// Getting the form from HTML
 var form = document.getElementById('form');
 
+var youtubediv = document.getElementById("youtubeDiv");
+
+
+// Form  of distance on Sumit
 form.onsubmit = getDistance;
+
 function getDistance(event) {
   event.preventDefault()
-  console.log(location)
+  //console.log(location)
   var origin = document.getElementById('start').value;
   destination = document.getElementById('end').value;
-  
+  // Distance Mtrix API
   service = new google.maps.DistanceMatrixService();
 
+  // Giving estential values.
   service.getDistanceMatrix({
     origins: [origin],
     destinations: [destination],
@@ -27,13 +34,25 @@ function getDistance(event) {
     if (status == "OK") {
       origin.value = response.destinationAddresses[0];
       destination.value = response.originAddresses[0];
-      dist.innerHTML = "Estimated time to travel "+response.rows[0].elements[0].duration.text;
-      console.log(response.rows[0].elements[0].duration.text)
+      dist.innerHTML = "Estimated time to travel " + response.rows[0].elements[0].distance.text + " is " + response.rows[0].elements[0].duration.text;
+      youtubediv.classList.remove("hidden");
+      youtubediv.classList.add("show");
+      var youtubeForm = document.getElementById('youtubeForm');
+
+      // For calling youtube
+      youtubeForm.onclick = getVideo;
+      // console.log(response.rows[0].elements[0].distance.text)
     } else {
       alert("Error: " + status);
     }
   }
-  
+
 }
+
+function getVideo(event) {
+  console.log("Inside the youtube block");
+}
+
+
 
 
