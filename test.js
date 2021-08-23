1e262dfa5a121330d26bcba5edfc869ef5ef428c
manalsolanki <97manal@gmail.com>
2021-08-23 14:43:59 -0400

    Added the design.

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 var form = document.getElementById('form');
 
 form.onsubmit = getDistance;
-
 function getDistance(event) {
   event.preventDefault()
   console.log(location)
-  var origin = document.getElementById('start').value
-  destination = document.getElementById('end').value
+  var origin = document.getElementById('start').value;
+  destination = document.getElementById('end').value;
+  
   service = new google.maps.DistanceMatrixService();
 
   service.getDistanceMatrix({
@@ -20,15 +20,14 @@ function getDistance(event) {
   );
 
   function callback(response, status) {
-    var orig = document.getElementById("orig"),
-      dest = document.getElementById("dest"),
-      dist = document.getElementById("dist");
-    console.log(response);
+    // var orig = document.getElementById("orig"),
+    //   dest = document.getElementById("dest"),
+    //   dist = document.getElementById("dist");
+    // console.log(response);
     if (status == "OK") {
-      console.log(response);
-      orig.value = response.destinationAddresses[0];
-      dest.value = response.originAddresses[0];
-      dist.innerHTML = response.rows[0].elements[0].duration.text;
+      origin.value = response.destinationAddresses[0];
+      destination.value = response.originAddresses[0];
+      dist.innerHTML = "Estimated time to travel "+response.rows[0].elements[0].duration.text;
       console.log(response.rows[0].elements[0].duration.text)
     } else {
       alert("Error: " + status);
