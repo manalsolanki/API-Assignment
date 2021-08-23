b444c4d849a2ef63faee664122b6ca20af222c09
Manal Solanki <97manal@gmail.com>
2021-08-23 12:38:23 -0400

    changes.

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
-var origin = "65 Alhart Drive",
-        destination = "6 Spring Valley Crt, Brampton",
-        service = new google.maps.DistanceMatrixService();
+var location = document.getElementById('location');
+console.log(location);
+location.onsubmit = getDistance;
 
-      service.getDistanceMatrix({
-          origins: [origin],
-          destinations: [destination],
-          travelMode: google.maps.TravelMode.DRIVING,
-          avoidHighways: false,
-          avoidTolls: false
-        },
-        callback
-      );
+function getDistance(event) {
+  event.preventDefault()
+  var origin = document.getElementById('start').value
+  destination = document.getElementById('end').value
+  service = new google.maps.DistanceMatrixService();
 
-      function callback(response, status) {
-        var orig = document.getElementById("orig"),
-          dest = document.getElementById("dest"),
-          dist = document.getElementById("dist");
-            console.log(response);
-        if (status == "OK") {
-            console.log(response);
-          orig.value = response.destinationAddresses[0];
-          dest.value = response.originAddresses[0];
-          dist.value = response.rows[0].elements[0].distance.text;
-          console.log(response.rows[0].elements[0].duration.text)
-        } else {
-          alert("Error: " + status);
-        }
-      }
+  service.getDistanceMatrix({
+    origins: [origin],
+    destinations: [destination],
+    travelMode: google.maps.TravelMode.DRIVING,
+    avoidHighways: false,
+    avoidTolls: false
+  },
+    callback
+  );
+
+  function callback(response, status) {
+    var orig = document.getElementById("orig"),
+      dest = document.getElementById("dest"),
+      dist = document.getElementById("dist");
+    console.log(response);
+    if (status == "OK") {
+      console.log(response);
+      orig.value = response.destinationAddresses[0];
+      dest.value = response.originAddresses[0];
+      dist.value = response.rows[0].elements[0].distance.text;
+      console.log(response.rows[0].elements[0].duration.text)
+    } else {
+      alert("Error: " + status);
+    }
+  }
+  
+}
+
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+https://developers.google.com/youtube/v3/getting-started
+
+https://www.youtube.com/watch?v=rzk8n-4gMWs
+
+https://developers.google.com/youtube/v3/docs/search/list?apix=true#type
+
+https://developers.google.com/youtube/v3/docs/search/list?apix=true#type
+
+https://developers.google.com/explorer-help/guides/code_samples#javascript
